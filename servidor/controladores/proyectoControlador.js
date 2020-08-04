95437d1c1c7857b679f6397d6f8e8f8a6bb088ac
Francisco Jarma <pancho24.1997@gmail.com>
2020-08-04 14:33:04 -0300

    Modificar proyectos en la bd terminada

--- servidor/controladores/proyectoControlador.js
+++ servidor/controladores/proyectoControlador.js
@@ -29,3 +29,45 @@ exports.obtenerProyectosPorUsuario = async(req,res) => {
         res.status(500).send('Hubo un error');
     }
 };
+//modificar proyectos
+exports.modificarProyecto = async(req,res) => {
+    //revisamos si hay errores, en caso de que haya lo colocamos en un array que mostrará dichos errores
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({ errores: errores.array()});
+    }
+    //array destructuring para sacar información del proyecto
+    const { nombre, fechaInicio, fechaFin, duracion } = req.body;
+    const nuevoProyecto = {};
+    if(nombre){
+        nuevoProyecto.nombre = nombre;
+    }
+    if(fechaInicio){
+        nuevoProyecto.fechaInicio = fechaInicio;
+    }
+    if(fechaFin){
+        nuevoProyecto.fechaFin = fechaFin;
+    }
+    if(duracion){
+        nuevoProyecto.duracion = duracion;
+    }
+    try {
+        //revisamos el ID del proyecto
+        let proyecto = await Proyecto.findById(req.params.id);
+        //revisamos si el proyecto existe o no
+        if(!proyecto){
+            return res.status(404).json({mensaje:'Proyecto no encontrado'});
+        }
+        //el proyecto existe, entonces debemos verificar que pertenezca a un usuario
+        if(proyecto.creador.toString() !== req.usuario.id){
+            return res.status(401).json({mensaje:'Usuario no autorizado'});
+        }
+        //modificamos el proyecto (le tenemos que pasar el id y definir el nuevo valor)
+        proyecto = await Proyecto.findByIdAndUpdate({_id: req.params.id}, { $set: nuevoProyecto},
+            { new: true});
+        res.json({proyecto});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
--- servidor/routes/proyectos.js
+++ servidor/routes/proyectos.js
@@ -6,17 +6,26 @@ const { check } = require('express-validator');
 //Crea proyectos
 //api/proyectos (endpoint o URL)
 router.post('/',
+    //lo primero que hacemos es autenticar si hay un usuario logueado
     auth,
     [
+    //hacemos la validación de la información
         check('nombre','El nombre del proyecto es obligatorio').not().isEmpty(),
         check('fechaInicio','Ingrese una fecha de inicio válida').isDate(),
         check('fechaFin','Ingrese una fecha de fin válida').isDate(),
         check('duracion','Ingrese una duración válida').isInt()
     ],
+    //llamamos a la función creada en el Controlador
     proyectoControlador.crearProyecto
 );
+//trae proyectos por usuario
 router.get('/',
     auth,
     proyectoControlador.obtenerProyectosPorUsuario
+);
+//modificar un proyecto
+router.put('/:id',
+    auth,
+    proyectoControlador.modificarProyecto
 );
 module.exports = router;
