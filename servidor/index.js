91a38716aca61a06e20ffca66ea8abf965d1aaa9
Francisco Jarma <pancho24.1997@gmail.com>
2020-09-19 17:07:10 -0300

    commit cliente

--- servidor/index.js
+++ servidor/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const conectarBaseDeDatos = require ('./config/db.js');
+const cors = require("cors-anywhere");
 //creamos el servidor
 const app = express();
-
-
-
 //conectamos a la bd
 conectarBaseDeDatos();
 //para leer datos que ingrese el usuario
 app.use(express.json({ extended: true}));
 
-
 //puerto de APP
 const port = process.env.PORT || 4000;
+
+cors.createServer({
+    originWhitelist: [],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2'] // permite que puedan acceder todos los orígenes de las peticiones
+});
 
 // Importamos rutas
 app.use('/api/usuarios', require('./routes/usuarios.js'));
@@ -22,6 +25,6 @@ app.use('/api/tareas', require('./routes/tareas.js'));
 
 //usamos API por si decidimos crear el proyecto web sin usar REST API. 
 //arrancamos la app
-app.listen(port, '0.0.0.0', ()=>{
+app.listen(port, ()=>{
     console.log(`El servidor está funcionando en el puerto ${port}`);
 });
